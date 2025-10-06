f6328b2a5855f7ff38b1cd0a7bb011e851190c41
anji suri <anji.suri.23@ucl.ac.uk>
2025-10-06 21:10:14 +0100

    Converting control data from MATLAB to .csv format (with pandas)

---
+++ ctrl_mat_to_python.py
@@ -0,0 +1,31 @@
+from scipy.io import loadmat
+import numpy as np
+import pandas as pd
+
+data = loadmat('/Users/anji/Desktop/lab project/eye_tracking_data/controls.mat', squeeze_me = True, struct_as_record = False)
+
+eye = np.asarray(data['eyeTrck']).squeeze()
+
+for i, subject in enumerate(eye, start = 1):
+    if subject is None:
+        print(f'Skipping row {i} (empty data)')
+        continue
+    else:
+        reshaped_data = np.transpose(subject, (1,2,0)).reshape(-1,3) # reorders axes (from channels, timepoints, trials to timepoints, trials, channels) 
+        df = pd.DataFrame (reshaped_data, columns = ['x', 'y', 'pupil'])
+        df.to_csv(f'/Users/anji/Desktop/lab project/python_data/controls/participant_{i}.csv', index = False)
+
+
+
+
+# xdiff = np.diff(trial1[0, :]) ** 2
+# ydiff = np.diff(trial1[1,:]) ** 2
+# speed = np.sqrt(xdiff + ydiff)
+# plt.figure(figsize=(10,4))
+# time = np.arange(len(speed)) / 200  # seconds
+# plt.plot(time, speed)
+# plt.title('Instantaneous eye movement speed (Trial 1)')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Speed (a.u.)')
+# plt.tight_layout()
+# plt.show()
--- init_data_vis.py
+++
@@ -1,6 +0,0 @@
-from scipy.io import loadmat
-from pathlib import Path
-
-base = ('/Users/anji/Desktop/lab project')
-loadmat((base+'/eye_tracking_data/controls'))
-loadmat((base+'/eye_tracking_data/patients'))
