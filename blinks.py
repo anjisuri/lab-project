e527f14e349543555bc35debcfda262e648ba819
anji suri <anji.suri.23@ucl.ac.uk>
2025-11-21 14:48:12 +0000

    added blinks.py

---
+++ blinks.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# plotting pupil dilation for a single trial (to identify eye blinks)
+def pupil_dilation(participant, trial_number):
+    fs = 200  # sampling frequency Hz
+    samples = 1601
+
+    # load npy file for participant
+    file_path = f'/Users/anji/Desktop/lab project/EyeData/controls/ctrl_{participant}.npy'
+    data = np.load(file_path)  # shape: (3, time, trials)
+
+    pupil_data = data[2]  # pupil is channel 2
+    trial_pupil = pupil_data[:, trial_number - 1]  # timepoints for this trial
+
+    time = np.arange(samples) / fs
+
+    plt.figure(figsize=(10,4))
+    plt.plot(time, trial_pupil)
+    plt.title(f'Pupil dilation: participant {participant} trial {trial_number}')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Pupil dilation (au)')
+    plt.tight_layout()
+    plt.show()
